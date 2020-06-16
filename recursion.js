dfc1bcbbf1d50aa6d263b2b208f0461aaa295595
Cullen Smith <cullensmi@gmail.com>
2020-06-16 08:16:39 -0400

    june16 commit of recursion.js

--- recursion.js
+++ recursion.js
@@ -13,3 +13,19 @@ and return a Boolean.
 Test it on 50 and 75. See how it behaves on -1. Why? Can you think of any way
 to fix this? */
 
+// function isEven(num){
+    
+//     if (num===2){
+//         console.log(num===2)}
+
+//     if (num===1){
+//         console.log(num===1)
+//     }
+    
+//     else num -= num;
+//     isEven(num);
+// }
+
+
+
+isEven(5);
