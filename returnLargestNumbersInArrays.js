dedb7ed938d4c199a42c8da45fbef223cde2de62
Cullen Smith <cullensmi@gmail.com>
2020-06-21 12:59:42 -0400

    june21 commit of algorithm

--- returnLargestNumbersInArrays.js
+++ returnLargestNumbersInArrays.js
@@ -3,15 +3,19 @@ Remember, you can iterate through an array with a simple for loop, and access ea
 
 
 function largestOfFour(arr) {
+    let newArr = [];
+    let finalArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr[i] =  arr[i].sort(function(b,a){
+            return a-b
+        })     
+    }         
+       for (let i = 0; i < newArr.length; i++) {
+        finalArr[i] = newArr[i][0];   
 
-    arr.sort(function(a,b){
-        return b-a
-         })
-
-
-   
-  }
+            }
+            return finalArr;
+}
   
-  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
-  
+  largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]);
 
