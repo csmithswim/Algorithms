7064cdace31492688decd07547102681821b4738
Cullen Smith <cullensmi@gmail.com>
2020-06-21 19:11:34 -0400

    june21 commit of algorithm

---
+++ confirmTheEnding.js
@@ -0,0 +1,15 @@
+/* Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead. */
+
+//If you loop 
+function confirmEnding(str, test) {
+
+// console.log(str.length)
+testStr = str.substring(1)
+// console.log(test)
+console.log(testStr);
+
+}
+
+confirmEnding("Bastian", "turkey");
