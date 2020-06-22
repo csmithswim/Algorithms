943f618e09427d24390390645af34ab79e2d7447
Cullen Smith <cullensmi@gmail.com>
2020-06-22 16:21:47 -0400

    june22 commit of algorithm

--- confirmTheEnding.js
+++ confirmTheEnding.js
@@ -2,14 +2,23 @@
 
 This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead. */
 
-//If you loop 
-function confirmEnding(str, test) {
+function confirmEnding(str, target) {
 
-// console.log(str.length)
-testStr = str.substring(1)
-// console.log(test)
-console.log(testStr);
+    if (target == str.substring(str.length-1) && target.substring(0,target.length-1) == str.substring(str.length-target.length, str.length-1)) {
 
+        console.log('true')
+
+        return true;
+    } else console.log('false')
+
+    return false;
 }
 
-confirmEnding("Bastian", "turkey");
+confirmEnding("Abstraction", "action");
+
+// let str = 'connor'
+// let target = 'n'
+
+// console.log(target.length)
+// console.log(target.substring(0,0))
+// console.log(str.substring(str.length-target.length, str.length-1))
