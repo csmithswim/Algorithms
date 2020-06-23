2f2139de3c6d60861f41b8ed3a6f4d863b043428
Cullen Smith <cullensmi@gmail.com>
2020-06-23 13:09:03 -0400

    june23 commit of algorithm

--- truncateAString.js
+++ truncateAString.js
@@ -1,7 +1,15 @@
 /* Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending. */ 
 
 function truncateString(str, num) {
+
+  let tStr = str.substring(0,num)+"...";
+  if (num >= str.length){
+  console.log(str);
   return str;
+  } else 
+  console.log(tStr)
+  return tStr;
+  
 }
 
-truncateString("A-tisket a-tasket A green and yellow basket", 8);
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length+2);
