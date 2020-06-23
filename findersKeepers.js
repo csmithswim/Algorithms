3244a08448a194a90d58767e918fb02b8d49df9d
Cullen Smith <cullensmi@gmail.com>
2020-06-23 15:23:06 -0400

    june23 commit of algorithm

--- findersKeepers.js
+++ findersKeepers.js
@@ -8,14 +8,34 @@ findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }) should return
 
 
 function findElement(arr, func) {
-    let num = 0;
+    let num = [];
+
     for (let i = 0; i < arr.length; i++) {
-        const element = array[i];
-        
+        num += arr[i];
+
+        if (func(num) == true) {
+
+            console.log(arr[i]);
+
+            return arr[i];             
+        }
     }
 
+    for (let i = 0; i < arr.length; i++) {
 
-  }
+        num += arr[i];
+
+        if (func(num) == false) {
+
+            console.log('undefined');
+
+            return undefined; 
+        }
+    }
+    
+}
+
+      
   
-  findElement([1, 2, 3, 4], num => num % 2 === 0);
+  findElement([1, 3, 5, 9], num => num % 2 === 0);
   
