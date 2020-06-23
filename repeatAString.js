b891f97623f45c4750a0a826cdce1a2376ca7414
Cullen Smith <cullensmi@gmail.com>
2020-06-22 23:12:05 -0400

    june22 commit of algorithm

---
+++ repeatAString.js
@@ -0,0 +1,21 @@
+/* Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. */ 
+
+
+function repeatStringNumTimes(str, num) {
+let emptyStr = '';
+
+    if (num<0) { 
+        
+    return emptyStr;
+    
+    } else {
+
+    
+}
+
+    return str;
+
+  }
+  
+  repeatStringNumTimes("abc", 3);
+  
