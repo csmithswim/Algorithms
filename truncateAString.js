ce8ce72f7d42b2ad68269a68c2e10d3f37bbac2f
Cullen Smith <cullensmi@gmail.com>
2020-06-22 23:42:18 -0400

    june22 commit of algorithm

--- truncateAString.js
+++ truncateAString.js
@@ -1,37 +1,7 @@
-/* Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
-*/
+/* Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending. */ 
 
-//Algorithm questions.
-//1. strings and intergers
-// 2. Input is strings and intergers and the output is just strings.Input
-// 3. I don't think so 
-// 4. Yes? Idk ILR 
-// 5. Idk
-// 6.
-// 7.
+function truncateString(str, num) {
+  return str;
+}
 
-//I need to compare str.length to num and truncate or pop off the remaining str.length + ...
-
-
-
-function truncateString(str, num){  
-    // let arr = [];
-    // arr += str; 
-    // newArr = arr.slice(0, num)
-    // console.log(newArr+"...");
-      
-  
-} 
-  function truncateString(str, num) {
-   if (str.length > num) {
-      return str.slice(0, num) + "...";
-    } else {
-      return str;
-    }
-  }
-   
-  
-  
-truncateString("Peter Piper picked a peck of pickled peppers", 11);
-
-  
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
