e7fd6f6871bf2567b8b4f40b65c55616f4a01432
Cullen Smith <cullensmi@gmail.com>
2020-06-25 10:42:59 -0400

    june25 commit of algorithm

--- sliceAndSplice.js
+++ sliceAndSplice.js
@@ -11,7 +11,7 @@ The second array should remain the same after the function runs.
 //?We are splicing arr1 into index n in arr2. We need to have a for loop iterate through and splice arr1 into arr2 at index n.
 
 function frankenSplice(arr1, arr2, n) {
-    let newArray = arr2.slice(0,2);
+    let newArray = arr2.slice();
     // console.log(newArray)
     for (let i = arr1.length-1; i >=  0; i--) {
 
@@ -26,5 +26,4 @@ function frankenSplice(arr1, arr2, n) {
     
   }
   
-frankenSplice([1, 2, 3], [4, 5], 1)
-
+  frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2)
