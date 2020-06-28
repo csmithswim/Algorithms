27c37d5d09edffba70cd52160fdc6ec76f601916
Cullen Smith <cullensmi@gmail.com>
2020-06-27 21:44:30 -0400

    algorithm completion and added explanation

--- whereDoIBelong.js
+++ whereDoIBelong.js
@@ -1,11 +1,24 @@
 /* Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number. For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1). Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1). 
 
+getIndexToIns([10, 20, 30, 40, 50], 35) should return 3.
+getIndexToIns([10, 20, 30, 40, 50], 35) should return a number.
+getIndexToIns([10, 20, 30, 40, 50], 30) should return 2.
+getIndexToIns([10, 20, 30, 40, 50], 30) should return a number.
+getIndexToIns([40, 60], 50) should return 1.
+getIndexToIns([40, 60], 50) should return a number.
+getIndexToIns([3, 10, 5], 3) should return 0.
+getIndexToIns([3, 10, 5], 3) should return a number.
+getIndexToIns([5, 3, 20, 3], 5) should return 2.
+getIndexToIns([5, 3, 20, 3], 5) should return a number.
+getIndexToIns([2, 20, 10], 19) should return 2.
+getIndexToIns([2, 20, 10], 19) should return a number.
 getIndexToIns([2, 5, 10], 15) should return 3.
 getIndexToIns([2, 5, 10], 15) should return a number.
 getIndexToIns([], 1) should return 0.
 getIndexToIns([], 1) should return a number.
-
 */ 
+
+//In this function we are taking an array and a number as arguments and comparing the number against the array to find out what index it would take if it were inserted into the array. First the array is sorted and then we use a for loop to iterate through the array going from lowest to highest, once the array is larger than the number we stop the loop from finishing by returning the iterator. We find the index by returning the length 
 
 function getIndexToIns(arr, num) {
   arr.sort(function(a, b) {
@@ -19,4 +32,4 @@ function getIndexToIns(arr, num) {
   return arr.length;
 }
 
-getIndexToIns([2, 5, 10], 15) 
+getIndexToIns([10, 20, 30, 40, 50], 35) 
