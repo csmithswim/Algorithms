1d549bbd3ddaed5a2417d48b9d79ad3e40596d54
Cullen Smith <cullensmi@gmail.com>
2020-07-03 13:33:12 -0400

    still working on new ideas

--- seekAndDestroy.js
+++ seekAndDestroy.js
@@ -32,21 +32,4 @@ destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugat
 // destroyer([1, 2, 3, 1, 2, 3], 2, 3) 
 
 
-function nextInLine(arr, item) {
-  // Only change code below this line
-  arr.push(item);
-  arr.shift();
 
-  return item;
-  // Only change code above this line
-  
-
-}
-
-// Setup
-var testArr = [1,2,3,4,5];
-
-// Display code
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
