e9cf20ddc11963d1c6301847a55ba8874ed44c2a
Cullen Smith <cullensmi@gmail.com>
2020-07-05 16:44:05 -0400

    added day function

--- hackerRank/HR.js
+++ hackerRank/HR.js
@@ -293,3 +293,14 @@ function getMaxLessThanK(n, k) {
     return maximum;
 }
 
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+    
+    var day_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    var date = new Date(dateString);
+    
+    dayName = day_names[date.getDay()];
+    
+    return dayName;
+}
