66e7e60c23a80183966f384d1ca483052695ef25
Cullen Smith <cullensmi@gmail.com>
2020-07-04 22:55:36 -0400

    added count object

--- hackerRank/HR.js
+++ hackerRank/HR.js
@@ -257,3 +257,15 @@ class Polygon {
     }
     
 }
+
+function getCount(objects) {
+    var count = 0;
+    for (var index in objects)
+        {
+            if (objects[index].x == objects[index].y)
+                {
+                    count++;
+                }
+        }
+    return count;
+}
