f0d66e5043dc59f6f22b38e047f44000af9f9e83
Cullen Smith <cullensmi@gmail.com>
2020-07-04 22:52:53 -0400

    add throw error algorithm

--- hackerRank/HR.js
+++ hackerRank/HR.js
@@ -176,4 +176,39 @@ function getGrade(score)
     return grade;
 }
 
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    var sorted_array = nums.sort(function (a,b) {return a - b;});
+    var unique_sorted_array = sorted_array.filter(function(elem, index, self) {
+        return index === self.indexOf(elem);
+    })
 
+    return unique_sorted_array[unique_sorted_array.length - 2];
+}
+
+
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+function isPositive(a) {
+    if (a > 0)
+        {
+            return "YES";
+        }
+    else if (a === 0)
+        {
+            throw new Error("Zero Error");
+        }
+    else if (a < 0)
+        {
+            throw new Error("Negative Error");
+        }
+}
