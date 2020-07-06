dbdbed955861045ebc04a53a623815ad385d2ca0
Cullen Smith <cullensmi@gmail.com>
2020-07-06 14:18:22 -0400

    added a conditional to finally solve one function call

--- seekAndDestroy.js
+++ seekAndDestroy.js
@@ -9,63 +9,32 @@ destroyer(["tree", "hamburger", 53], "tree", 53) should return ["hamburger"].
 destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan") should return [12,92,65].
 */
 
-// function destroyer(arr, ...nums) {
+function destroyer(arr, ...nums) {
+    let newArr = [];
+    for (let i = 0; i < nums.length; i++) {
 
-//   function testValues(arr){
+        newArr[i] = nums[i];
+    }
 
-//   if arr < 10 {
+    for (let i = 0; i < arr.length; i++) {
 
+        if (arr.includes(nums[i])){
 
-//   }
-    
-    
-//   }
- 
-// }
+            arr.splice(arr.indexOf(nums[i]),1)
+        }
+        if (arr.includes(nums[i])){
 
-//   const result = arr.filter(testValues)
+            arr.splice(arr.indexOf(nums[i]),1)
+        }
+    }
+    // if (vowels.indexOf(s[i]) > -1)
 
-//   console.log(result)
+    console.log(arr)
 
-// }
-
-// destroyer([1, 2, 3, 1, 2, 3], 2, 3) 
-
-
-
-/*
-golfScore(4, 2) should return "Eagle"
-golfScore(5, 2) should return "Eagle"
-golfScore(4, 3) should return "Birdie"
-golfScore(4, 4) should return "Par"
-golfScore(1, 1) should return "Hole-in-one!"
-golfScore(5, 5) should return "Par"
-golfScore(4, 5) should return "Bogey"
-golfScore(4, 6) should return "Double Bogey"
-golfScore(4, 7) should return "Go Home!"
-golfScore(5, 9) should return "Go Home!"
-
-
-Strokes	Return
-1	"Hole-in-one!"
-<= par - 2	"Eagle"
-par - 1	"Birdie"
-par	"Par"
-par + 1	"Bogey"
-par + 2	"Double Bogey"
->= par + 3	"Go Home!"
-
-var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
-*/
-
-function golfScore(par, strokes) {
-  // Only change code below this line
-
-
-  return "Change Me";
-  // Only change code above this line
 }
 
-golfScore(5, 4);
+destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) 
+
+
 
 
