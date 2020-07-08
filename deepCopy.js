d900376315a7526553d02c4cb19401b510db890d
Cullen Smith <cullensmi@gmail.com>
2020-07-08 09:42:05 -0400

    recursive algorithm

---
+++ deepCopy.js
@@ -0,0 +1,34 @@
+let testObj = {
+    a: "test",
+    b: 3,
+    c: { d: "testing", e: 7 },
+    d: { nested: { fun() {console.log('hi')} } }
+    };
+
+
+
+
+
+
+
+
+
+
+
+// deepCo464py = (obj) => {
+// let newObj = {};
+    
+    
+//    newObj = JSON.parse(JSON.stringify(obj))
+
+//    newObj.d.nested = {d:"new"};
+// console.log(testObj)
+// console.log(newObj);
+// return newObj;
+
+// }
+
+// deepCopy(testObj)
+// console.log(testObj)
+
+
