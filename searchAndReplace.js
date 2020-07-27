a6ed7023075c6947eea0853826cce98770952266
Cullen Smith <cullensmi@gmail.com>
2020-07-27 12:46:12 -0400

    solved algorithm, will add better solutions later

--- searchAndReplace.js
+++ searchAndReplace.js
@@ -6,19 +6,40 @@ Perform a search and replace on the sentence using the arguments provided and re
 
 
 function myReplace(str, before, after) {
-  
-  let newStr = str.split(" ");
-  console.log(newStr);
-    return str;
-  }
-  
-  
-  myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+let newStr = str.split(" ");
 
-/*
-myReplace("Let us go to the store", "store", "mall") should return "Let us go to the mall".
-myReplace("He is Sleeping on the couch", "Sleeping", "sitting") should return "He is Sitting on the couch".
-myReplace("This has a spellngi error", "spellngi", "spelling") should return "This has a spelling error".
+let regex = /[A-Z]/;
+
+  if (before[0].match(regex)){
+    let capLet = after[0].toUpperCase()
+    let partialStr = after.substring(1)
+    let finalAfterStr = capLet.concat(partialStr);
+
+      newStr.splice(newStr.indexOf(before), 1, finalAfterStr)
+
+      let finalStr = newStr.join(' ');
+
+  console.log(finalStr)
+
+  return finalStr
+
+    } else 
+
+    newStr.splice(newStr.indexOf(before), 1, after)
+
+    let finalStr = newStr.join(' ');
+
+    console.log(finalStr)
+
+    return finalStr;
+}
+
+
+// myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+// myReplace("He is Sleeping on the couch", "Sleeping", "sitting") should return "He is Sitting on the couch".
+myReplace("This has a spellngi error", "spellngi", "spelling")  /* should return "This has a spelling error".
 myReplace("His name is Tom", "Tom", "john") should return "His name is John".
 myReplace("Let us get back to more Coding", "Coding", "algorithms") should return "Let us get back to more Algorithms".
+myReplace("Let us go to the store", "store", "mall") should return "Let us go to the mall".
+
 */
