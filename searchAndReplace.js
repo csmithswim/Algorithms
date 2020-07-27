86731e6eed7640608e15f3576da6a49555d17be0
Cullen Smith <cullensmi@gmail.com>
2020-07-26 23:51:53 -0400

    updated algorithm

--- searchAndReplace.js
+++ searchAndReplace.js
@@ -1,22 +1,17 @@
 /* 
-
-
-Perform a search and replace on the sentence using the arguments provided and return the new sentence.
-
-First argument is the sentence to perform the search and replace on.
-
-Second argument is the word that you will be replacing (before).
-
-Third argument is what you will be replacing the second argument with (after).
-
-Note
-Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
-
+Perform a search and replace on the sentence using the arguments provided and return the new sentence. First argument is the sentence to perform the search and replace on. Second argument is the word that you will be replacing (before). Third argument is what you will be replacing the second argument with (after).Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
 */ 
 
+//Todo split the first argument into an array, and then splice the third argument into where the second argument is in the first array.adminBtns
+
+
 function myReplace(str, before, after) {
+  
+  let newStr = str.split(" ");
+  console.log(newStr);
     return str;
   }
+  
   
   myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
 
@@ -26,6 +21,4 @@ myReplace("He is Sleeping on the couch", "Sleeping", "sitting") should return "H
 myReplace("This has a spellngi error", "spellngi", "spelling") should return "This has a spelling error".
 myReplace("His name is Tom", "Tom", "john") should return "His name is John".
 myReplace("Let us get back to more Coding", "Coding", "algorithms") should return "Let us get back to more Algorithms".
-
-
 */
