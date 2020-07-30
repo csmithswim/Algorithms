6b48de124917830a2f66f3ffe67cf4c04e78ed2a
Cullen Smith <cullensmi@gmail.com>
2020-07-29 23:06:26 -0400

    updated algoritm

--- missingLetters.js
+++ missingLetters.js
@@ -1,14 +1,15 @@
-/* 
-
-Find the missing letter in the passed letter range and return it.
-
-If all letters are present in the range, return undefined.
-
-*/
+/* Find the missing letter in the passed letter range and return it.
+If all letters are present in the range, return undefined.*/
 
 function fearNotLetter(str) {
+
+  var patt1 = /[^?a-z]/g;
+  var result = str.match(patt1);
+  console.log(result)
+  //Use a regex to find when the pattern breaks and return the missing letter
+
     return str;
-  }
+  } 
   
   fearNotLetter("abce");
 
