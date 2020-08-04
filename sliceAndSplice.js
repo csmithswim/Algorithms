59e64de5569115165ba2e75995bfe8ee73df90e1
Cullen Smith <cullensmi@gmail.com>
2020-08-04 18:42:34 -0400

    another run through sliceandsplice

--- sliceAndSplice.js
+++ sliceAndSplice.js
@@ -1,29 +1,27 @@
-/* You are given two arrays and an index. Use the array methods slice and splice to copy each element of the first array into the second array, in order. Begin inserting elements at index n of the second array. Return the resulting array. The input arrays should remain the same after the function runs.
+/* You are given two arrays and an index. Copy each element of the first array into the second array, in order. Begin inserting elements at index n of the second array. Return the resulting array. The input arrays should remain the same after the function runs. */
 
-frankenSplice([1, 2, 3], [4, 5], 1) should return [4, 1, 2, 3, 5].
+
+//TODO Insert first array into index n of second array with a for loop and return the array, both input arrays must remain the same 
+
+
+function frankenSplice(arr1, arr2, n) {
+
+  for (let i = 0; i < arr1.length; i++) {
+
+    arr2.splice(n, 0, arr1[i])
+
+  }
+
+  console.log(arr2)
+  return arr2;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+
+/*frankenSplice([1, 2, 3], [4, 5], 1) should return [4, 1, 2, 3, 5].
 frankenSplice([1, 2], ["a", "b"], 1) should return ["a", 1, 2, "b"].
 frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2) should return ["head", "shoulders", "claw", "tentacle", "knees", "toes"].
 All elements from the first array should be added to the second array in their original order.
 The first array should remain the same after the function runs.
-The second array should remain the same after the function runs.
-*/ 
-
-//?We are splicing arr1 into index n in arr2. We need to have a for loop iterate through and splice arr1 into arr2 at index n.
-
-function frankenSplice(arr1, arr2, n) {
-    let newArray = arr2.slice();
-    // console.log(newArray)
-    for (let i = arr1.length-1; i >=  0; i--) {
-
-    newArray.splice(n,0,arr1[i]);
-        
-    }
-
-   
-    console.log(arr2)
-    console.log(newArray)
-    return newArray;
-    
-  }
-  
-  frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2)
+The second array should remain the same after the function runs. */
