75ec5d1719924b39319cfdc6ccf2ea966c2e9a14
Cullen Smith <cullensmi@gmail.com>
2020-08-11 19:51:47 -0400

    finished algorithm

--- sliceAndSplice.js
+++ sliceAndSplice.js
@@ -1,22 +1,24 @@
 /* You are given two arrays and an index. Copy each element of the first array into the second array, in order. Begin inserting elements at index n of the second array. Return the resulting array. The input arrays should remain the same after the function runs. */
 
-
 //TODO Insert first array into index n of second array with a for loop and return the array, both input arrays must remain the same 
-
 
 function frankenSplice(arr1, arr2, n) {
 
+  let finalArray = arr2.slice(0,arr2.length);
+  
   for (let i = arr1.length - 1; i >= 0; i--){
 
-    arr2.splice(n, 0, arr1[i])
+   finalArray.splice(n, 0, arr1[i])
         
   }
 
-  console.log(arr2);
+  console.log(finalArray)
+  console.log(arr2)
+  return finalArray; 
 
 }
 
-frankenSplice([1, 2, 3], [4, 5], 1);
+frankenSplice([1, 2], ["a", "b"], 1);
 
 
 /*frankenSplice([1, 2, 3], [4, 5], 1) should return [4, 1, 2, 3, 5].
