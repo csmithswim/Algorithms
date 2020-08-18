e92d38044f6f1af90c258d9e1750d6141cddff9a
Cullen Smith <cullensmi@gmail.com>
2020-08-18 19:03:55 -0400

    taking another run with this algorithm

--- seekAndDestroy.js
+++ seekAndDestroy.js
@@ -1,33 +1,45 @@
-/* You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments. 
-Note you have to use the arguments object.
+/* You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
 
-** Passes with code so far
+Note
+You have to use the arguments object. */
 
-**destroyer(["tree", "hamburger", 53], "tree", 53) should return ["hamburger"].
-**destroyer([1, 2, 3, 1, 2, 3], 2, 3) should return [1, 1].
-**destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) should return [1, 5, 1].
-**destroyer([3, 5, 1, 2, 2], 2, 3, 5) should return [1].
-**destroyer([2, 3, 2, 3], 2, 3) should return [].
 
-destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan") should return [12,92,65].
-*/
+function destroyer(...arr) {
 
-function destroyer(arr, ...nums) {
-       
-    const loop = arr.length;
+    let newArray1 = arr[0].slice(0,arr[0].length)
+    let newArray2 = arr.slice(1,arr.length)
+    let finalArray = [];
 
-    for (let i = 0; i <= loop; i++) {
+    for (let i = 0; i < newArray.length; i++) {
 
-        if (arr.includes(nums[i])){
+        // console.log(newArray[i])
+        // console.log(arr[i])
 
-            arr.splice(arr.indexOf(nums[i]),1)
 
-            i = i - 1
-        }       
+      if (newArray[i] !== arr[i]){
+
+        console.log(newArray[i])
+        console.log(arr[i])
+
+        finalArray.push(newArray[i])        
+      }        
+
     }
-    
-    return arr;
+
+        console.log(newArray2)
 }
+  
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 
-destroyer([2, 3, 2, 3], 2, 3)
+ //   } else {
+    
 
+    //   }
+  
+
+  /*destroyer([1, 2, 3, 1, 2, 3], 2, 3) should return [1, 1].
+destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) should return [1, 5, 1].
+destroyer([3, 5, 1, 2, 2], 2, 3, 5) should return [1].
+destroyer([2, 3, 2, 3], 2, 3) should return [].
+destroyer(["tree", "hamburger", 53], "tree", 53) should return ["hamburger"].
+destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan") should return [12,92,65]. */
