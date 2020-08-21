f1862b0f3ea7ed8788d2bd14b8e305220888fd64
Cullen Smith <cullensmi@gmail.com>
2020-08-21 15:58:56 -0400

    added some more attempts

--- findTheSymmetricDifference.js
+++ findTheSymmetricDifference.js
@@ -2,55 +2,32 @@
 Symmetric difference is a binary operation, which means it operates on only two elements. So to evaluate an expression involving symmetric differences among three elements (A △ B △ C), you must complete one operation at a time. Thus, for sets A and B above, and C = {2, 3}, A △ B △ C = (A △ B) △ C = {1, 4} △ {2, 3} = {1, 2, 3, 4}.
 Create a function that takes two or more arrays and returns an array of their symmetric difference. The returned array must contain only unique values (no duplicates). */
 function sym(...args) {
-
-    // console.log(args[0].indexOf(3, 3))
-
-    // let newTest;
-
-    let newArgs = args.slice(0,args.length).sort(function(a,b){return b.length-a.length});
-
-    // testArgArray = newArgs.slice(0,1).reduce((sum, elem) => sum + elem)
-
-    let finalArray = [];    
-
     // newTest = testArgArray.reduce((sum, elem) => sum + elem)
+    let repVal = [];
+    let finalArray = []; 
+    let newArgs = args.slice(0,args.length).sort(function(a,b){return b.length-a.length});
+    testArgArray = newArgs.slice(0,1).reduce((sum, elem) => sum + elem)
 
-    // let sum = sample.reduce((sum, elem) => sum + elem) //Reduce, sum is last value returned
+    for (let i = 0; i < testArgArray.length; i++) {
 
+      if (testArgArray.indexOf(testArgArray[i]) !==  testArgArray.indexOf(testArgArray[i], i+1) &&
+            
+      testArgArray[i] === testArgArray[i+1]){
 
-    for (let i = 0; i <= newArgs.length-1; i++) {
-
-        // console.log(newArgs.length)
-
-        for (let j = 0; j < newArgs[i].length; j++) {
-
-            // console.log(newArgs[i][j])
-
-            // console.log(finalArray.indexOf(newArgs[i][j]))
-    
-            if (finalArray.indexOf(newArgs[i][j]) == -1){
-
-                finalArray.splice(0,0,newArgs[i][j])
-
-            } else if (finalArray.indexOf(newArgs[i][j]) >= 0 && finalArray.indexOf(newArgs[i][j], j) >= j)  {
-
-                finalArray.splice(finalArray.indexOf(newArgs[i][j]),1)
-
-                // finalArray.length = finalArray.length - 1;
-            }   
-        }
-    }
-    
-
+        testArgArray.splice(testArgArray.indexOf(testArgArray[i]), 1)
+      }
 
   
-console.log(newArgs)
 // return finalArray;
-console.log(finalArray)
+// console.log(repVal)
+
+    }
+    console.log(testArgArray)
+
 
 }
   
-sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]);
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]);
 
 /* 
 sym([1, 2, 3], [5, 2, 1, 4]) should return [3, 4, 5].
@@ -66,7 +43,44 @@ sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should contain only three elements
 sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should return [2, 3, 4, 6, 7].
 sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should contain only five elements.
 sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should return [1, 2, 4, 5, 6, 7, 8, 9].
-sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should contain only eight elements. 
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should contain only eight elements. .
+
+
+
+
+    for (let i = 0; i < newArgs.length; i++) {
+
+        newArgs[i].sort(function(a,b){return a-b})        
+    }    
+
+    for (let i = 0; i <= newArgs.length-1; i++) {
+
+        // console.log(newArgs.length)
+
+        for (let j = 0; j < newArgs[i].length; j++) {
+
+            // console.log(newArgs[i][j])
+
+            // console.log(finalArray.indexOf(newArgs[i][j]))
+
+            // console.log(finalArray)
+
+            // console.log('newArgs is '+ newArgs[i][j])
+
+            // console.log('j is '+ j)
+    
+            if (finalArray.indexOf(newArgs[i][j]) == -1){
+
+                finalArray.splice(0,0,newArgs[i][j])
+
+            } else if (finalArray.indexOf(newArgs[i][j]) > finalArray.indexOf(newArgs[i][j], finalArray.indexOf(newArgs[i][j] + 1)))  {
+
+                // repVal.push(newArgs[i][j])
+
+                finalArray.splice(finalArray.indexOf(newArgs[i][j]),1)
+            }   
+        }
+    }
 
  */
 
