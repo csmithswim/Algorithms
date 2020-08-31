874d92741ae07cd60702b3745a579ac40bebd826
Cullen Smith <cullensmi@gmail.com>
2020-08-31 18:47:09 -0400

    solved algorithm

--- missingLetters.js
+++ missingLetters.js
@@ -1,25 +1,37 @@
 /* Find the missing letter in the passed letter range and return it.
 If all letters are present in the range, return undefined.*/
 
-function fearNotLetter(str) {
+const fearNotLetter = (str) => {
+  let missingLetterDigit;
+  let missingLetter;
 
-  var patt1 = /[^?a-z]/g;
-  var result = str.match(patt1);
-  console.log(result)
-  //Use a regex to find when the pattern breaks and return the missing letter
+  for (let i = 0; i < str.length-1; i++) {
 
-    return str;
-  } 
-  
-  fearNotLetter("abce");
+    // console.log(str.charCodeAt(i+1) - str.charCodeAt(i))
 
-  
-/*
+    if (str.charCodeAt(i+1) - str.charCodeAt(i) !== 1 ){
 
-fearNotLetter("abce") should return "d".
+      missingLetterDigit = str.charCodeAt(i) + 1;
+      missingLetter = String.fromCharCode(missingLetterDigit);
+      
+      // console.log(str.charCodeAt(i))
+        
+    }
+
+  }
+
+  console.log(missingLetterDigit);
+  console.log(missingLetter);
+  return missingLetter;
+}
+
+fearNotLetter("abce") 
+/*should return "d".
 fearNotLetter("abcdefghjklmno") should return "i".
 fearNotLetter("stvwx") should return "u".
 fearNotLetter("bcdf") should return "e".
 fearNotLetter("abcdefghijklmnopqrstuvwxyz") should return undefined.
-
 */ 
+
+
+// && isNaN(str.charCodeAt(i+1) - str.charCodeAt(i)) === false Using isNaN to test for NaN
