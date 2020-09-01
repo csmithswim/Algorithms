9976afc5d8a6ce44b3f06e7560780c671d5c7cb4
Cullen Smith <cullensmi@gmail.com>
2020-09-01 19:18:09 -0400

    solved all but one case

--- sumAllOddFibonacciNumbers.js
+++ sumAllOddFibonacciNumbers.js
@@ -2,15 +2,46 @@
 For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5. */ 
 
 function sumFibs(num) {
-    return num;
-  }
+   let fib = [1];
+   let finalFib=0;
+    for (let i = 1; i < num; i++) {
+     
+    if (i < 2){        
+    fib.push(i);
+    } else if (i > 2){ 
+
+    fib.push(fib[i-2]+fib[i-3])
+    } 
+              
+    }
+    console.log(fib);         
+    
+    for (let i = 0; i <= fib.length && fib[i] <= num; i++) {
+
+        if ((fib[i] - 1) % 2 == 0){
+
+
+            // console.log('test')
+
+            finalFib += (fib[i]);
+        } 
+
+        // console.log(fib[i])
+        
+    }
+    
+    // console.log(finalFib);
+   
+    return finalFib;
+}
   
-  sumFibs(4);
-
-
-sumFibs(1000) 
-
+// sumFibs(4);
+sumFibs(4) 
 /* should return 1785.
+
+
+
+
 sumFibs(1) should return a number.
 sumFibs(4000000) should return 4613732.
 sumFibs(4) should return 5.
