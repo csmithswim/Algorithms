e80d242ce25671b5431743694303163cff1c4ff5
Cullen Smith <cullensmi@gmail.com>
2020-09-15 18:31:45 -0400

    solved algorithm

--- whereforeArtThou.js
+++ whereforeArtThou.js
@@ -2,70 +2,40 @@
 For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
 */
 function whatIsInAName(collection, source) {
-    //Variable to collect the collection & source matches
-    let finalArray = [];
-    let newCollection;
-    //Entry length declaration
-
-    let sourceLength = Object.entries(source).length;
-
-    // console.log(sourceLength)
-
-    //Push the values of the collection objects into an array of arrays
-    let keyArray = [];
-    let valueArray = [];
-    for (let i = 0; i < collection.length; i++) {
-        keyArray.push(Object.keys(collection[i]))        
-        valueArray.push(Object.values(collection[i]))
-    }
-    // console.log(collection)+console.log('\n');
-    // console.log(keyArray)+console.log('\n');
-    // console.log(valueArray)+console.log('\n');
-    // console.log(source)+console.log('\n');
-
-    // console.log(sourceLength)
-    //Loops to iterate through collection to compare source entries
-    for (let i = 0; i < collection.length; i++) {
-        for (let j = 0; j <= sourceLength-1; j++) {
-            // console.log(collection[i])
-            // console.log(Object.keys(source))
-
-            // console.log(collection[i].hasOwnProperty(Object.keys(source)))  
-
-            // console.log((collection[i][Object.keys(source)] == Object.values(source)))
-
-            if (collection[i].hasOwnProperty(Object.keys(source)) && collection[i][Object.keys(source)] == Object.values(source)){
-
-                console.log(collection[i])
-                finalArray.push(collection[i])
-            }
-
-            // console.log(collection[i][Object.keys(source)])
-
-            // console.log(Object.values(collection[i]))  
-            // console.log(Object.values(source))      
-            // console.log(Object.values(collection[i]).hasOwnProperty(Object.values(source)))  
+    let sourceProp =  Object.keys(source);
+    let x=0;
+    let result = [];
     
-        }        
+    for (var j=0; j<collection.length;j++) {
+
+        for (var i=0; i<sourceProp.length;i++) {
+      
+            if (collection[j].hasOwnProperty(sourceProp[i]) && collection[j][sourceProp[i]] == source[sourceProp[i]]) {
+      
+            x++;
+        }     
     }
+    // check if the all keys exist with their values
+    if (x==sourceProp.length ) {
+      result.push(collection[j]);
+    }
+    x=0;
+    }  
+    console.log(result)
+    return result;
+  }
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 })
 
-    console.log(finalArray)
-    return finalArray;
-}
-  
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }) 
-
-/* should return [{ first: "Tybalt", last: "Capulet" }] */
-
+// should return [{ "apple": 1, "bat": 2, "cookie": 2 }].
 
 /*
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }) 
 should return [{ first: "Tybalt", last: "Capulet" }].
 
 whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 }) 
 should return [{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }].
 
-whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }, { "bat":2 }], { "apple": 1, "bat": 2 }) 
-should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie":2 }].
+
 
 whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3}) 
 should return [] */
