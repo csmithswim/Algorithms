0494f51f9722d9ec88e3ef988aa0d9959a824384
Cullen Smith <cullensmi@gmail.com>
2020-09-18 18:30:21 -0400

    solved algorithm

--- smallestCommonMultiple.js
+++ smallestCommonMultiple.js
@@ -1,43 +1,33 @@
 /*Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters. The range will be an array of two numbers that will not necessarily be in numerical order. For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6. */
 
 function smallestCommons(arr) {
-//First sort arr in descending order
-arr.sort(function(a,b){
-  return b-a})
-  // console.log(arr)     
+  //First sort arr in descending order
+  arr.sort(function(a,b){
+  return a-b})
 
-  //Set variable for output
-  let answer=1;
+  //Define global variables
+  let maxNum = arr[arr.length-1];
+  let minNum = arr[0];
 
-  //Get array of the range of numbers
-  let array = [];
-  for (let i = arr[0]; i >= arr[1]; i--) {
-    array.push(i)    
+  let lcm = maxNum;
+  // console.log(maxNum-1)
+  // console.log(minNum)
+
+  for (let i = maxNum - 1; i >= minNum; i--) {    
+
+    // console.log(i)
+
+    if (lcm % i){
+      lcm += maxNum;
+      // console.log(i)
+      i = maxNum;
+    }    
+    
   }
-  //Loop to iterate through the array and multiply the indexes and then check for even divisibility with the other indexes
-// let product = array[0];
-
-let finalArray = array.slice(0, array.length)
-let newArray=[];
-
-  for (let i = 0; i <= array.length-1; i++) {
-
-    for (let j = 0; j <= 10; j++){
-      // console.log(finalArray[i] % array[i] != 0)  
-      console.log(finalArray)    
-      console.log(finalArray[i])
-
-      // if (finalArray[i] % array[i] != 0){
-      //   console.log(array[i])
-      // }
-      finalArray[i] += array[i]
-
-    }
-   
+  console.log(lcm) 
+  return lcm;
 }
-  console.log(finalArray)
-}
-smallestCommons([1,5]);
+smallestCommons([23,18]);
 /*
 smallestCommons([1, 5]) should return a number. 
 smallestCommons([1, 5]) should return 60.
@@ -46,24 +36,22 @@ smallestCommons([2, 10]) should return 2520.
 smallestCommons([1, 13]) should return 360360. 
 smallestCommons([23, 18]) should return 6056820. */
 
-  // Highest to lowest in loop
-  // for (let i = 0; i < array.length; i++) {
 
-  //   if (product % array[i] !==0){
-  //     console.log(array[i])
-  //     console.log(product)
-  //     product = product * array[i]
-  //     console.log(product)
-  //   }            
-  // }
-  //Lowest to highest
-  // for (let i = array.length-1; i >= 0; i--){
-  
-  //   if (product % array[i] != 0){
-  //     console.log(product)
-  //     product = product * array[i];
-  //     console.log(array[i]) 
-  //   }
-  // }
-  // console.log(product)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
