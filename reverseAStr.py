7a4a7c86ff696c9594527f53ea7504862021b157
Cullen Smith <cullensmi@gmail.com>
2020-09-22 19:12:01 -0400

    first commit

---
+++ reverseAStr.py
@@ -0,0 +1,18 @@
+def reverseAString():
+    word = input('Enter word to be reversed:\n')
+
+    for element in word:
+        finalStr = ''
+        finalStr = element + finalSt
+        print(finalStr)
+
+
+    #One way to reverse a string in python uses a slice that steps backward
+
+    # print(word[::-1])
+    # return word[::-1]
+
+
+
+
+reverseAString()
