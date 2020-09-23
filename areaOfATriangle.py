8ad0a8b8a15ab479545dd149c7a1e18125b6bf7a
Cullen Smith <cullensmi@gmail.com>
2020-09-23 10:32:36 -0400

    solved algorithm

--- areaOfATriangle.py
+++ areaOfATriangle.py
@@ -1,4 +1,5 @@
 #Write a functiont hat takes the base and height of a triangle and return its area
 
 def area(base,height):
-    return area = (base*height)/2
+    area = (base*height)/2
+    return area
