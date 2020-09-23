89f14cbd3fa24530a5b6163d53eca0dec0639163
Cullen Smith <cullensmi@gmail.com>
2020-09-23 09:45:57 -0400

    first commit

---
+++ maximumEdgeOfATriangle.py
@@ -0,0 +1,9 @@
+#Create a functiont hat finds that maximum range of a traignle's third edge, where the side lengths are all integers.
+
+def maxEdge(x,y):
+    z = (x+y)-1
+    print(z)
+    return z
+    
+
+maxEdge(8,10)
