cc3ebb99c898a0bb4756f6e59f73838929eff91d
Cullen Smith <cullensmi@gmail.com>
2020-09-24 16:22:53 -0400

    solved algorithm

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,2 +1,24 @@
 #Create a function that takes a number as an argument and returns "Fizz", "Buzz" or "FizzBuzz".
+# If the number is a multiple of 3 the output should be "Fizz".
+# If the number given is a multiple of 5, the output should be "Buzz".
+# If the number given is a multiple of both 3 and 5, the output should be "FizzBuzz".
+# If the number is not a multiple of either 3 or 5, the number should be output on its own as shown in the examples below.
+# The output should always be a string even if it is not a multiple of 3 or 5.
 
+
+def fizzBuzz():
+    num=int(input("Input number to convert to Fizz/Buzz/FizzBuzz \n"))
+    finalMessage=''
+    if num % 3 == 0 :
+        finalMessage += 'Fizz'
+        # print('Fizz')
+    if num % 5 == 0:
+        finalMessage +='Buzz'
+        # print('Buzz')
+    # else: print(str(num))
+    # print(num)
+    print(finalMessage or str(num))
+
+
+
+fizzBuzz()
