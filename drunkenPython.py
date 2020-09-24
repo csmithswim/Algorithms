1d51c080b4edbfb4cf0f8f129ce032069dec9b0f
Cullen Smith <cullensmi@gmail.com>
2020-09-24 14:15:52 -0400

    first commit

---
+++ drunkenPython.py
@@ -0,0 +1,15 @@
+#Write two functions, one that converts a str to int and one that converts a int to str
+str=input("Please put in a string to be converted to a number ")
+
+def strToInt(str):
+    int=int(str)
+    print(int)
+
+
+
+def inTtoStr(int):
+
+
+
+strToInt(input)
+intToStr(input)
